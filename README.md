27f1017b59eb8951703becc5d982de61b4348f7b
Valters Grisans <valters.grisans@gmail.com>
2017-07-30 23:07:09 +0300

    Added temperature and hardware list

--- README.md
+++ README.md
@@ -3,7 +3,22 @@
 ## Description
 Homemade HomeKit device using a lot of cables but giving that joy when you create something with your own hands. A small demo here: 
 
-[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/YJR_WMAwMMw/0.jpg)](https://www.youtube.com/watch?v=YJR_WMAwMMw)
+[![HomeKit Demo](https://img.youtube.com/vi/YJR_WMAwMMw/0.jpg)](https://www.youtube.com/watch?v=YJR_WMAwMMw)
+
+Screenshot from iPhone:
+
+![HomeKit screenshot](http://screens.valters.me/jjGhfiBc9j.png)
+
+
+## Hardware list
+- Apple TV 3rd gen (Works as your HomeKit HUB)
+- Router (I use ASUS RT-N12+)
+- iPhone (for obvious reasons)
+- Arduino Uno
+- HR911105A Ethernet Shield
+- DHT11 temperature sensor
+- Raspberry Pi model B+ v1.2
+- Lot of cables (ethernet and arduino)
 
 ## Installation
 
@@ -30,16 +45,31 @@ After you've installed HomeBridge on Pi:
       "on_url": "http://192.168.2.202:80/?on",
       "off_url": "http://192.168.2.202:80/?off",
       "http_method": "GET"
+    },
+    {
+      "accessory": "HttpTemphum",
+      "name": "Living Room Weather",
+      "url": "http://192.168.2.202/?weather",
+      "http_method": "GET"
+    },
+    {
+      "accessory": "OpenweathermapTemperature",
+      "name": "Outside Temperature",
+      "url": "http://api.openweathermap.org/data/2.5/weather?lat=56.932900&lon=24.076831&APPID=YOURAPPID"
     }
   ]
 }
 ```
-5. Run HomeBridge. There could be some problems with running HomeBridge globally on Pi, so I run it using this ```/opt/node/lib/node_modules/homebridge/bin/homebridge```
+5. Install ```npm install -g homebridge-http```
+6. Install ```npm install -g homebridge-openweathermap-temperature```
+7. Install ```npm install -g homebridge-httptemperaturehumidity```
+8. Run HomeBridge. There could be some problems with running HomeBridge globally on Pi, so I run it using this ```/opt/node/lib/node_modules/homebridge/bin/homebridge```
 
 
 ### Arduino
 I've uploaded Arduino INO file to this repository. My Arduino setup:
 - Arduino Uno 
 - HR911105A Ethernet Shield (In the beginning tried with ESP8266, but it was too much hustle)
-- SRD-05VDC-SL-C relay module connected on pin 2
+- SRD-05VDC-SL-C relay module connected on pin 3
+- DHT11 temperature and humidity module connected on pin 2
 - A desk lamp connected to relay module
--- _01_desklamp-arduino-v1/_01_desklamp-arduino-v1.ino
+++ _01_desklamp-arduino-v1/_01_desklamp-arduino-v1.ino
@@ -9,8 +9,13 @@ byte subnet[] = { 255, 255, 255, 0 };
 EthernetServer server(80);
 String readString; 
 
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup(){  
-  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
   
   Ethernet.begin(mac, ip, gateway, gateway, subnet);
   server.begin();
@@ -25,48 +30,59 @@ void loop(){
       if (client.available()) {
         char c = client.read();
 
-        if (readString.length() < 100) {
+        if (readString.length() < 20) {
           readString += c; 
         } 
         
         if (c == '\n') {
-
-          // Serial.println(readString);
-
           client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println();
-
-          client.println("<HTML>");
-          client.println("<HEAD>");
-          client.println("<TITLE>Desk Lamp web switch</TITLE>");
-          client.println("</HEAD>");
-          client.println("<BODY>");
-
-          client.println("<H1>Desk Lamp web switch</H1>");
           
-          client.println("<a href=\"/?on\">ON</a>"); 
-          client.println("<a href=\"/?off\">OFF</a>"); 
+          if(readString.indexOf("weather") != -1) {
+            
+            float h = dht.readHumidity();
+            float t = dht.readTemperature();
+            float hic = dht.computeHeatIndex(t, h, false);
+            
+            client.println("Content-Type: application/json");
+            client.println();
+            client.print("{\n \"temperature\":");
+            client.print(t);
+            client.print(",\n \"humidity\":");
+            client.print(h);
+            client.print("\n}");
 
-          client.println("</BODY>");
-          client.println("</HTML>");
+            delay(1);
+  
+            client.stop();
+            
+          } else {
+            client.println("Content-Type: text/html");
+            client.println();
+  
+            client.println("<HTML>");
+            client.println("<HEAD>");
+            client.println("<TITLE>Desk Lamp web switch</TITLE>");
+            client.println("</HEAD>");
+            client.println("<BODY>");
+  
+            client.println("<H1>Desk Lamp web switch</H1>");
+            
+            client.println("<a href=\"/?on\">ON</a>"); 
+            client.println("<a href=\"/?off\">OFF</a>"); 
+  
+            client.println("</BODY>");
+            client.println("</HTML>");
+  
+            delay(1);
+  
+            client.stop();
 
-          delay(1);
-
-          client.stop();
-          
-          if(readString.indexOf("on") != -1)
-          {
-            // Serial.println(readString.indexOf("on"));            
-            digitalWrite(2, LOW);
-            // Serial.println("Led On");
-
-          }
-          if(readString.indexOf("off") != -1)
-          {
-            // Serial.println(readString.indexOf("off"));
-            digitalWrite(2, HIGH);
-            // Serial.println("Led Off");
+            if(readString.indexOf("on") != -1) {          
+              digitalWrite(3, LOW);
+            }
+            if(readString.indexOf("off") != -1) {
+              digitalWrite(3, HIGH);
+            }
           }
           readString="";
 
