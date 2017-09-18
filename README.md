64ccc64bd3f0bfa760079f9055318372a721fa5d
Valters Grisans <valters.grisans@gmail.com>
2017-09-19 01:25:06 +0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HomeKit Desk Lamp made with Raspberry Pi and Arduino
+# Latvian HomeKit made with Raspberry Pi and Arduino
 
 ## Description
 Homemade HomeKit device using a lot of cables but giving that joy when you create something with your own hands. A small demo here: 
@@ -16,9 +16,12 @@ Screenshot from iPhone:
 - iPhone (for obvious reasons)
 - Arduino Uno
 - HR911105A Ethernet Shield
-- DHT11 temperature sensor
-- Raspberry Pi model B+ v1.2
+- DHT22 temperature sensor
+- WS2812B RGB LED ring
+- Keyestudio Single Channel Relay
+- Raspberry Pi 3
 - Lot of cables (ethernet and arduino)
+- Soldering iron
 
 ## Installation
 
@@ -33,8 +36,8 @@ After you've installed HomeBridge on Pi:
 {
   "bridge": {
     "name": "Homebridge",
-    "username": "CC:22:3D:E3:CE:30",
-    "port": 51826,
+    "username": "CC:22:3D:E3:CE:31",
+    "port": 51826, 
     "pin": "031-45-154"
   },
   "platforms": [],
@@ -42,8 +45,8 @@ After you've installed HomeBridge on Pi:
     {
       "accessory": "Http",
       "name": "Desk Lamp",
-      "on_url": "http://192.168.2.202:80/?on",
-      "off_url": "http://192.168.2.202:80/?off",
+      "on_url": "http://192.168.2.202:80/?lamp_on",
+      "off_url": "http://192.168.2.202:80/?lamp_off",
       "http_method": "GET"
     },
     {
@@ -53,17 +56,36 @@ After you've installed HomeBridge on Pi:
       "http_method": "GET"
     },
     {
-      "accessory": "OpenweathermapTemperature",
-      "name": "Outside Temperature",
-      "url": "http://api.openweathermap.org/data/2.5/weather?lat=56.932900&lon=24.076831&APPID=YOURAPPID"
+      "accessory": "HttpTemphum",
+      "name": "Outside Weather",
+      "url": "http://192.168.2.202/?w_out",
+      "http_method": "GET"
+    },
+    {
+      "accessory": "HTTP-RGB",
+      "name": "RGB",
+      "switch": {
+        "status": "http://192.168.2.202/?rgb_status",
+        "powerOn": "http://192.168.2.202/?rgb_on",
+        "powerOff": "http://192.168.2.202/?rgb_off"
+      },
+      "brightness": {
+        "status": "http://192.168.2.202/?rgb_brightness",
+        "url": "http://192.168.2.202/?rgb_light/%s"
+      },
+      "color": {
+        "status": "http://192.168.2.202/?rgb_set",
+        "url": "http://192.168.2.202/?rgb_hue/%s",
+        "brightness": true
+      }
     }
   ]
 }
 ```
-5. Install ```npm install -g homebridge-http```
-6. Install ```npm install -g homebridge-openweathermap-temperature```
-7. Install ```npm install -g homebridge-httptemperaturehumidity```
-8. Run HomeBridge. There could be some problems with running HomeBridge globally on Pi, so I run it using this ```/opt/node/lib/node_modules/homebridge/bin/homebridge```
+5. Install ```$ npm install -g homebridge-http```
+6. Install ```$ npm install -g homebridge-httptemperaturehumidity```
+7. Install ```$ npm install -g homebridge-better-http-rgb```
+8. Run HomeBridge ```$ homebridge```
 
 
 ### Arduino
@@ -71,5 +93,5 @@ I've uploaded Arduino INO file to this repository. My Arduino setup:
 - Arduino Uno 
 - HR911105A Ethernet Shield (In the beginning tried with ESP8266, but it was too much hustle)
 - SRD-05VDC-SL-C relay module connected on pin 3
-- DHT11 temperature and humidity module connected on pin 2
+- DHT22 temperature and humidity module connected on pin 2 (inside) and on pin 4 (outside)
 - A desk lamp connected to relay module
--- _01_desklamp-arduino-v1/_01_desklamp-arduino-v1.ino
+++ _01_desklamp-arduino-v1/_01_desklamp-arduino-v1.ino
@@ -11,19 +11,44 @@ String readString;
 
 #include "DHT.h"
 #define DHTPIN 2
-#define DHTTYPE DHT11  
+#define DHTTYPE DHT22  
 DHT dht(DHTPIN, DHTTYPE);
+DHT dht_out(4, DHTTYPE);
+
+int R = 5;
+int G = 6;
+int B = 7;
+int stateR;
+int stateG;
+int stateB;
+
+int red;
+int blue;
+int green;
+
+// RING
+#include "HCWS2812.h"
+HCWS2812 HCWS2812;
 
 void setup(){  
   pinMode(3, OUTPUT);
+
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
   
   Ethernet.begin(mac, ip, gateway, gateway, subnet);
   server.begin();
 
   Serial.begin(9600); 
+
+  // RING
+  HCWS2812.SetBG(0, 0, 0);
+  HCWS2812.ClearBuffer();
 }
 
 void loop(){
+
   EthernetClient client = server.available();
   if (client) {
     while (client.connected()) {
@@ -55,34 +80,106 @@ void loop(){
   
             client.stop();
             
-          } else {
-            client.println("Content-Type: text/html");
-            client.println();
-  
-            client.println("<HTML>");
-            client.println("<HEAD>");
-            client.println("<TITLE>Desk Lamp web switch</TITLE>");
-            client.println("</HEAD>");
-            client.println("<BODY>");
-  
-            client.println("<H1>Desk Lamp web switch</H1>");
+          }
+          if(readString.indexOf("w_out") != -1) {
             
-            client.println("<a href=\"/?on\">ON</a>"); 
-            client.println("<a href=\"/?off\">OFF</a>"); 
-  
-            client.println("</BODY>");
-            client.println("</HTML>");
-  
+            float h = dht_out.readHumidity();
+            float t = dht_out.readTemperature();
+            float hic = dht_out.computeHeatIndex(t, h, false);
+            
+            client.println("Content-Type: application/json");
+            client.println();
+            client.print("{\n \"temperature\":");
+            client.print(t);
+            client.print(",\n \"humidity\":");
+            client.print(h);
+            client.print("\n}");
+
             delay(1);
   
             client.stop();
-
-            if(readString.indexOf("on") != -1) {          
-              digitalWrite(3, LOW);
+            
+          }
+          if(readString.indexOf("rgb_status") != -1) { 
+            client.println("Content-Type: text/html");
+            client.println();
+            stateR = digitalRead(5);
+            stateG = digitalRead(6);
+            stateB = digitalRead(7);
+            Serial.println(stateR);
+            Serial.println(stateG);
+            Serial.println(stateB);
+            if(stateR == 1 || stateG == 1 || stateB == 1) {
+              client.println("1");
+            } else {
+              client.println("0");
             }
-            if(readString.indexOf("off") != -1) {
-              digitalWrite(3, HIGH);
-            }
+            client.stop();
+          }
+          if(readString.indexOf("rgb_on") != -1) { 
+            client.println("Content-Type: text/html");
+            client.println();
+            digitalWrite(5, HIGH);
+            digitalWrite(6, HIGH);
+            digitalWrite(7, HIGH);
+            HCWS2812.SetBG(255, 255, 255);
+            client.println("1");
+            client.stop();
+          }
+          if(readString.indexOf("rgb_off") != -1) { 
+            client.println("Content-Type: text/html");
+            client.println();
+            digitalWrite(5, LOW);
+            digitalWrite(6, LOW);
+            digitalWrite(7, LOW);
+            client.println("0");
+            client.stop();
+          }
+          if(readString.indexOf("rgb_brightness") != -1) { 
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println("0");
+            client.stop();
+          }
+          if(readString.indexOf("rgb_set") != -1) { 
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println("ffffff");
+            client.stop();
+          }          
+          if(readString.indexOf("rgb_light") != -1) { 
+            client.println("Content-Type: text/html");
+            client.println();
+            Serial.println(readString.substring(16,19));
+            client.println(readString.substring(16,19));
+            client.stop();
+          }
+          if(readString.indexOf("rgb_hue") != -1) { 
+            String hexstring = readString.substring(14,20);
+            long number = (long) strtol( &hexstring[0], NULL, 16 );
+            int red = number >> 16;
+            int green = number >> 8 & 0xFF;
+            int blue = number & 0xFF;
+            setColor(red, green, blue);
+            Serial.println(readString.substring(14,20));
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println(readString.substring(14,20));
+            client.stop();
+          }
+          if(readString.indexOf("lamp_on") != -1) {          
+            digitalWrite(3, HIGH);
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println("lamp on");
+            client.stop();
+          }
+          if(readString.indexOf("lamp_off") != -1) {
+            digitalWrite(3, LOW);
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println("lamp off");
+            client.stop();
           }
           readString="";
 
@@ -91,3 +188,28 @@ void loop(){
     }
   }
 } 
+
+void setColor(int red, int green, int blue)
+{
+//  #ifdef COMMON_ANODE
+//    red = 255 - red;
+//    green = 255 - green;
+//    blue = 255 - blue;
+//  #endif
+//  analogWrite(R, red);
+//  analogWrite(G, green);
+//  analogWrite(B, blue);  
+
+  int index;
+  
+  /* Fill the output buffer with random colours */
+  for(index = 0; index < 16; index++)
+  {  
+    RGBBuffer[RED][index] = red;
+    RGBBuffer[GREEN][index] = green;
+    RGBBuffer[BLUE][index] = blue;
+  }
+  
+  //HCWS2812.SetBG(red, green, blue);
+  HCWS2812.Refresh();
+}
